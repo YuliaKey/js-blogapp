212a5deab4955cf2d1b71ca5e5ccdeb719bf8739
Yuliia Kolomiiets <jk182013@gmail.com>
2023-01-20 12:03:34 +0100

    feat: suppression des articles

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -600,22 +600,46 @@ const displayArticles = articles => {
         <p class="article-author">${article.author}</p>
         <p class="article-content">${article.content}</p>
         <div class="article-actions">
-            <button class="btn btn-danger" data.id=${article._id}>Supprimer</button>
+            <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
         </div>`;
     return articleNode;
   });
   articlesContainer.innerHTML = '';
   articlesContainer.append(...articlesDOM);
+  const deleteBtns = articlesContainer.querySelectorAll(".btn-danger"); //recupere tous les boutons
+
+  deleteBtns.forEach(button => {
+    button.addEventListener('click', async event => {
+      event.preventDefault();
+      try {
+        const target = event.target;
+        const articleId = target.dataset.id;
+        const response = await fetch(`https://restapi.fr/api/dwwm_yuliia2/${articleId}`, {
+          method: 'DELETE'
+        }); //im sending request to api to delete an article which was targeted to be deleted (when you click on button delete on the website)
+        const body = await response.json();
+        fetchArticles();
+        console.log(body);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
 };
 const fetchArticles = async () => {
+  //fonction asynchrone qui recupere les donnes depuis l"API
   try {
     const response = await fetch("https://restapi.fr/api/dwwm_yuliia2");
     const articles = await response.json(); // json sends us the body of our response (as a list!) and we stock it in const articles
 
-    if (!articles.length) {
-      displayArticles([articles]);
+    if (articles.length !== 0) {
+      if (!articles.length) {
+        displayArticles([articles]);
+      } else {
+        displayArticles(articles);
+      }
     } else {
-      displayArticles(articles);
+      articlesContainer.innerHTML = "<p>Pas d'articles...</p>";
     }
   } catch (error) {
     console.log(error);
